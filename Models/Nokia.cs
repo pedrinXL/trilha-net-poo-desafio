21531a9ef1bcd28334d92868c63d5f6e9d28047b
pedrinXL <82837588+pedrinXL@users.noreply.github.com>
2023-10-30 16:44:13 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+         public override void InstalarAplicativo(string nome)
+        {
+            // Implemente o código para instalar o aplicativo no iPhone aqui
+            Console.WriteLine($"Instalando aplicativo {nome} no Nokia.");
+        }
     }
 }
