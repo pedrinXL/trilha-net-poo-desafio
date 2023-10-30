f2700904ed90a3d6a55fcbaffd18364f4cdd4d83
pedrinXL <82837588+pedrinXL@users.noreply.github.com>
2023-10-30 16:44:58 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone mac = new Iphone("993065613", "Iphone 14", "123456789", 8000);
+mac.InstalarAplicativo("discord");
+mac.Ligar();
+
+
+Nokia nk = new Nokia("988191359", "nk16", "258369147", 4000);
+nk.InstalarAplicativo("CS GO");
+nk.ReceberLigacao();
